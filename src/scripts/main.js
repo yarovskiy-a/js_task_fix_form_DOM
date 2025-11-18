e5edea2de4d0aed310cd4c86d29123de93b7ecb2
Yarovskiy Andrii <yarovskiy@gmail.com>
2025-11-18 14:33:51 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+
+const serchFields = document.getElementsByClassName('field');
+
+for (const oneFild of serchFields) {
+  const input = oneFild.querySelector('input');
+  let placeholderName = input.name;
+  let inputId = input.id;
+
+  placeholderName = placeholderName.toLocaleLowerCase();
+
+  placeholderName =
+    placeholderName.charAt(0).toUpperCase() + placeholderName.slice(1);
+  input.placeholder = placeholderName;
+
+  const newLabel = document.createElement('label');
+
+  oneFild.prepend(newLabel);
+  newLabel.textContent = placeholderName;
+  newLabel.classList.add('field-label');
+
+  if (!inputId) {
+    inputId = input.name + 'suffix';
+  }
+  newLabel.setAttribute('for', inputId);
+}
